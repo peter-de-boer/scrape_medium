d27af1097bd82883a87e67e5329f7b73a87c0359
Peter de Boer <peterdeboer_private@hotmail.com>
2019-11-06 11:59:38 +0100

    add comments

--- scrape_medium/spiders/medium.py
+++ scrape_medium/spiders/medium.py
@@ -7,26 +7,13 @@ import datetime
 
 class MediumSpider(scrapy.Spider):
 
-    # searches articles on medium.com
-    # with searchString in the title
+    # searches articles on medium.com with searchString in the title
     # writes results in fileName (csv format)
 
     name = 'medium'
     autothrottle_enabled=True  # just let's be nice :)
 
     def start_requests(self):
-
-        # searchString can be passed by:
-        #   scrapy crawl medium -a searchString=<mySearchString>
-
-        # if searchString not passed, set to empty string
-        try:
-            self.searchString
-        except:
-            self.searchString=''
-        self.queryString = self.searchString
-        self.queryString.replace(" ", "%20")
-        self.queryString = "%22" + self.queryString + "%22"
 
         # fileName can be passed by:
         #   scrapy crawl medium -a fileName=<myFileName>
@@ -37,12 +24,42 @@ class MediumSpider(scrapy.Spider):
         except:
             self.fileName='out.csv'
 
+        # searchString can be passed by:
+        #   scrapy crawl medium -a searchString=<mySearchString>
+
+        # if searchString not passed, set to empty string
+        try:
+            self.searchString
+        except:
+            self.searchString=''
+
+        # The searchString is used in two steps.
+        # In the first step it is passed as a url parameter.
+        # For this purposes, special characters need to be replaced.
+        # For a full reference, see. e.g. https://krypted.com/utilities/html-encoding-reference/
+        # We replace only spaces for now (add other characters when needed)
+        # Furthermore we put the searchString between double quotes.
+        # This is to ensure that in the case of multiple words (separated by
+        # spaces), medium.com only returns titles that match the whole string.
+
+        self.queryString = self.searchString
+        self.queryString.replace(" ", "%20")
+        self.queryString = "%22" + self.queryString + "%22"
+
 
         # medium.com has the feature that all request can be returned in json format
         # let's make use of it
+
         self.url = 'https://medium.com/search/posts?q=' + self.queryString + '&format=json'
 
-        #set header info by looking to Network Tab in Developer tools
+        # In order to handle the pagination on medium.com (it will only return
+        # 10 results at once), subsequent scrapy request need to be executed.
+        # This needs to be done by POST request though.
+        # For those requests, a header needs to be sent.
+        # We need to define the header only once. 
+        # The folllowing is basically copied from the header info in Network Tab in Developer tools
+        # in Chrome while browsing medium.com.
+
         self.headers= { \
         "sec-fetch-mode": "cors", \
         "origin": "https://medium.com", \
@@ -74,7 +91,7 @@ class MediumSpider(scrapy.Spider):
             print('error ', x.errno, ',', x.strerror)
             return
 
-        # header should be consistent with the features written in parse method
+        # csv header should be consistent with the features written in parse method
         if writeHeader:
             header = 'searchString,id,title,createdAt,firstPublishedAt,readingTime,wordCount,totalClapCount\n'
             self.file.write(header)
@@ -83,10 +100,15 @@ class MediumSpider(scrapy.Spider):
 
         yield scrapy.Request(self.url,callback=self.parse)
 
+        # Closing the file explicitly leads to problems related to multiple 
+        # open/close calls due to subsequent scrapy requests.
+        # So do not close explicitly, it will be automatically closed when the script ends
+
         #self.file.close()
 
 
     def storeFeatures(self, features):
+        # write features list as a comma separated line
         stringToWrite = ''
         for feature in features:
             stringToWrite = stringToWrite + str(feature) + ','
@@ -100,15 +122,22 @@ class MediumSpider(scrapy.Spider):
 
     def parse(self, response):
 
+        # get the text from the response
+
         response_data=response.text
+
         # strip of security code from json response
+
         response_split=response_data.split("while(1);</x>")
         response_string=response_split[1]
 
         # now convert the json string to true json object
+
         response_json=json.loads(response_string)
 
         # check if we have a valid non-empty result
+        # if not valid, just return
+
         if 'payload' not in response_json:
             return
         payload = response_json['payload']
@@ -117,15 +146,20 @@ class MediumSpider(scrapy.Spider):
             return
 
         # iterate over results and store
+
         for article in payload['value']:
 
-            # query returns not only original articles, but also responses
-            # to filter them out: it seems that original articles have empty
+            # The query returns not only original articles, but also responses to
+            # articles.
+            # To filter them out: it seems that original articles have empty
             # values for this key:
             #   "inResponseToPostId": ""
-            # the following key seems to be empty if inResponseToPostId is
-            # empty as well and vice versa, so we do not check for it:
-            #   "inResponseToMediaResourceId": "",
+            #
+            # Furthermore, the query also return articles where the subtitle
+            # matches the searchString (this is just how medium.com search
+            # works).
+            # To filter these out, check explicitly if the searchString indeed
+            # occurs in the title.
 
             if article["inResponseToPostId"]=="" and self.searchString.lower() in article["title"].lower():
 
@@ -133,9 +167,9 @@ class MediumSpider(scrapy.Spider):
                 print(self.count, article['id'], article['title'])
 
                 # store article
-                # first remove the following characters from title in order to 
+                # first remove the following characters from title in order to
                 # avoid problems with those characters in csv:
-                    #   ; , " ' + . : ? \n
+                #   ; , " ' + . : ? \n
                 title = article['title'].replace('"','')
                 title = title.replace(',','')
                 title = title.replace("'",'')
@@ -149,9 +183,9 @@ class MediumSpider(scrapy.Spider):
                 createdAt = self.getDateTime(article['createdAt'])
                 firstPublishedAt = self.getDateTime(article['firstPublishedAt'])
                 # format readingTime so fractions won't be converted to dates
-                # in excel
+                # in excel, in case you'd like to open the csv in excel
                 readingTime = f"{article['virtuals']['readingTime']:.3f}"
-                # features should be consistent with header written in
+                # featuresToStore should be consistent with header written in
                 # start_requests method
                 featuresToStore = [ \
                                    self.searchString, \
@@ -172,19 +206,29 @@ class MediumSpider(scrapy.Spider):
         if not 'next' in paging:
             return
 
+        # a next page is there, create a new request to get it
+
         body = json.dumps(paging['next'])
         yield scrapy.Request( self.url, method='POST', body=body, headers=self.headers, callback=self.parse )
 
 
 
+
 class PublicationXmlSpider(scrapy.Spider):
+
+    # searches on publications on medium with their own domain
+    # by parsing the xml file https://<publication domain/sitemap/sitemap.xml
+    # this is not finished due to problem swith xml parsing (see
+    # MediumXmlSpider)
+
+    name = 'mediumxml'
     name = 'publicationxml'
     autothrottle_enabled=True  # just let's be nice :)
 
     def start_requests(self):
 
         # searchString can be passed by:
-        #   scrapy crawl publicationxml -a ssearchString=<mySearchString>
+        #   scrapy crawl publicationxml -a searchString=<mySearchString>
 
         try:
             self.searchString
@@ -234,8 +278,19 @@ class PublicationXmlSpider(scrapy.Spider):
 
 
 class MediumXmlSpider(scrapy.Spider):
+
+    # searches on medium.com with searchString in the title
+    # by parsing the xml file https://medium.com/sitemap/sitemap.xml
+    # this is not completely finished
+    # usefulness is limited anyway: due to the number of requests and the fact
+    # that a download_delay between requests is needed, a typcial search would
+    # take weeks or months....
+
     name = 'mediumxml'
     autothrottle_enabled=True  # just let's be nice :)
+
+    # set a sufficient delay between request, else medium.com will deny
+    # requests
 
     custom_settings = {
         'DOWNLOAD_DELAY': 0.50
@@ -282,6 +337,7 @@ class MediumXmlSpider(scrapy.Spider):
         xmlposts = response.meta['url'] + 'sitemap/posts/'
         xlr =[u for u in xl if xmlposts in u]
 
+        # now parse the post xml files
         for url in xlr:
             yield scrapy.Request(url, callback=self.parse_postxml)
 
@@ -296,6 +352,7 @@ class MediumXmlSpider(scrapy.Spider):
         # get the list of urls
         xl=xxs.select('//n:url/n:loc/text()').extract()
 
+        # for each post, get the corresponding json data and parse them
         for url in xl:
             urljson = url + '?format=json'
             yield scrapy.Request(urljson, callback=self.parse_json)
